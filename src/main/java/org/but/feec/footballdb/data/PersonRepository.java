63953980e32de5189f08320bbd222c179edef054
Maxim Gura <maksim.gura.2000@mail.ru>
2021-12-24 17:08:42 +0100

    Establishing a connection to the database
    Establishing a connection to the database

--- src/main/java/org/but/feec/footballdb/data/PersonRepository.java
+++ src/main/java/org/but/feec/footballdb/data/PersonRepository.java
@@ -1,4 +1,38 @@
 package org.but.feec.footballdb.data;
 
+import org.but.feec.footballdb.api.*;
+import org.but.feec.footballdb.config.DataSourceConfig;
+import org.but.feec.footballdb.exceptions.DataAccessException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonRepository {
+
+    public PersonAuthView findPersonByEmail(String email) {
+        try (Connection connection = DataSourceConfig.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT email, pwd" +
+                             " FROM bds.person p" +
+                             " WHERE p.email = ?")
+        ) {
+            preparedStatement.setString(1, email);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return mapToPersonAuth(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Find person by ID with addresses failed.", e);
+        }
+        return null;
+    }
+
+    private PersonAuthView mapToPersonAuth(ResultSet rs) throws SQLException {
+        PersonAuthView person = new PersonAuthView();
+        person.setEmail(rs.getString("email"));
+        person.setPassword(rs.getString("pwd"));
+        return person;
+    }
 }
--- src/main/java/org/but/feec/footballdb/services/AuthService.java
+++ src/main/java/org/but/feec/footballdb/services/AuthService.java
@@ -1,6 +1,9 @@
 package org.but.feec.footballdb.services;
 
 import org.but.feec.footballdb.data.PersonRepository;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.but.feec.footballdb.exceptions.SearchException;
+import org.but.feec.footballdb.api.PersonAuthView;
 
 public class AuthService {
 
@@ -9,5 +12,23 @@ public class AuthService {
     public AuthService(PersonRepository personRepository)
     {
         this.personRepository = personRepository;
+    }
+
+    private PersonAuthView findPersonByEmail(String email) {
+        return personRepository.findPersonByEmail(email);
+    }
+
+    public boolean authenticate(String username, String password) {
+        if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
+            return false;
+        }
+
+        PersonAuthView personAuthView = findPersonByEmail(username);
+        if (personAuthView == null) {
+            throw new SearchException("Provided username is not found.");
+        }
+
+        BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), personAuthView.getPassword());
+        return result.verified;
     }
 }
